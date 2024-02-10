43dd247e62a9df59694cd3983f11afa40c4c7cab
alexaled <alexaled@gmail.com>
2024-02-10 22:31:17 +0100

    trunk

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button variant='outline' size='lg'>
+    <Button variant="outline" size="lg">
       Click it
     </Button>
   );
